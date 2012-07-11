b7a8c15f00d07bb94b2304219cd029a77d8ad3f9
Alert MicroStrategy <mstr.alert@gmail.com>
2012-07-10 20:56:59 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arvind::Application.config.secret_token = '3a38e9081e5b2026db1f1a03b391209269bd559ee4b18efb150208f80a759fe81828a9f47d9eaa72f4fa68644fed8e250aedf9ad66dd3d0bdbf998141de62924'
